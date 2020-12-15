90b132b7fb1f79932ffd3bfa39da6883f0481784
elifozerr <elifozerr1@outlook.com>
2020-12-15 15:51:26 +0300

    ChestCard and ChanceCard added(duty method is not implemented)

---
+++ MonopolyGameTest/src/sample/ChanceCard.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class ChanceCard extends Card{
+
+    public ChanceCard(int id){
+        this.id = id;
+        content = "Chance Card Duty: ";
+    }
+
+    @Override
+    public void duty() {
+
+    }
+}
