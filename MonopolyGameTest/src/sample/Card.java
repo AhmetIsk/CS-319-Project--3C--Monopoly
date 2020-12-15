ae7d927e161789f8b427ae283359ccf4090b6697
elifozerr <elifozerr1@outlook.com>
2020-12-15 15:31:18 +0300

    abstract Card class implementation

---
+++ MonopolyGameTest/src/sample/Card.java
@@ -0,0 +1,24 @@
+package sample;
+
+public abstract class Card {
+    String content;
+    int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public abstract void duty();
+}
