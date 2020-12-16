da5cfa04dc9b885ccc38b9f8e0b6a4d882970b99
Mehmet Yiğit Harlak <yigit.harlak@ug.bilkent.edu.tr>
2020-12-16 23:54:55 +0300

    Alien and Banker classes added

---
+++ MonopolyGameTest/src/sample/Alien.java
@@ -0,0 +1,45 @@
+package sample;
+
+public class Alien {
+
+    private int alienId;
+    private String alienName;
+    private String duty;
+
+    public int getAlienId() {
+        return this.alienId;
+    }
+
+    /**
+     *
+     * @param alienId
+     */
+    public void setAlienId(int alienId) {
+        this.alienId = alienId;
+    }
+
+    public String getAlienName() {
+        return this.alienName;
+    }
+
+    /**
+     *
+     * @param alienName
+     */
+    public void setAlienName(String alienName) {
+        this.alienName = alienName;
+    }
+
+    public String getDuty() {
+        return this.duty;
+    }
+
+    /**
+     *
+     * @param duty
+     */
+    public void setDuty(String duty) {
+        this.duty = duty;
+    }
+
+}
